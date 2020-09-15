df0ef00df45ba102613ee8981fd94bcb3d2bcca9
Bence Fulop <fbence90@gmail.com>
2020-09-15 16:54:55 +1000

    MAP: html and controller functions progress

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -2,16 +2,25 @@ class ProducersController < ApplicationController
     before_action :set_producers, only: [:show]
     skip_before_action :authenticate_user!, only: [:show, :index]
 
-    def index
-        @producers = Producer.all
-    end
+  def index
+    @producers = Producer.all
 
-    def show
+    @markers = @producers.geocoded.map do |producer|
+      {
+        lat: producer.latitude,
+        lng: producer.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { producer: producer }),
+        # image_url: helpers.asset_url('logo.png')
+      }
     end
+  end
 
-    private
+  def show
+  end
 
-    def set_producers
-        @producer = Producer.find(params[:id])
-    end
+  private
+
+  def set_producers
+    @producer = Producer.find(params[:id])
+  end
 end
