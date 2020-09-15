6d5255912f34e8729f6e60bc1c58faee6373d6bd
Lily Tan <lilyt2995@gmail.com>
2020-09-15 14:45:41 +1000

    ARCH: set up Producers Controller for index and show

---
+++ app/controllers/producers_controller.rb
@@ -0,0 +1,17 @@
+class ProducersController < ApplicationController
+    before_action :set_producers, only: [:show, :index]
+    skip_before_action :authenticate_user!, only: [:show, :index]
+
+    def index
+        @producers = Producer.all
+    end
+
+    def show
+    end
+
+    private
+
+    def set_producers
+        @producer = Producer.find(params[:id])
+    end
+end
