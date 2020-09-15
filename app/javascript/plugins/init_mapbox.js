8568b1a2d2f554b8cd063c32715e6d6e11dc39ed
Bence Fulop <fbence90@gmail.com>
2020-09-15 18:13:39 +1000

    FIX: mapbox JS code

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,20 +1,3 @@
-// import mapboxgl from 'mapbox-gl';
-
-// const initMapbox = () => {
-//   const mapElement = document.getElementById('map');
-
-//   if (mapElement) {
-//     // only build a map if there's a div#map to inject into
-//     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-//     const map = new mapboxgl.Map({
-//       container: 'map',
-//       style: 'mapbox://styles/mapbox/streets-v10',
-//     });
-//   }
-// };
-
-// export { initMapbox };
-
 import mapboxgl from 'mapbox-gl';
 
 const mapElement = document.getElementById('map');
@@ -36,7 +19,7 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
 const initMapbox = () => {
