03f8befc9c1f43f4aa92859d33cad2126ff4586a
Lily Tan <lilyt2995@gmail.com>
2020-09-15 16:42:10 +1000

    index boilerplate

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,5 +1,5 @@
 class ProducersController < ApplicationController
-    before_action :set_producers, only: [:show, :index]
+    before_action :set_producers, only: [:show]
     skip_before_action :authenticate_user!, only: [:show, :index]
 
     def index
