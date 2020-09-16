6eb191efe196eb8c11b593ec8b25a9775c792682
Lily Tan <lilyt2995@gmail.com>
2020-09-16 10:58:25 +1000

    modified toggle map

--- app/javascript/components/init_toggle_map.js
+++ app/javascript/components/init_toggle_map.js
@@ -1,9 +1,15 @@
-const mapToggle = document.querySelector("display-map");
-const listToggle = document.querySelector("display-list");
+const mapToggle = document.querySelector(".display-map");
+const listToggle = document.querySelector(".display-list");
+const producerList = document.getElementById("js-producers")
+const map = document.getElementById("js-map")
 
-const initToggleMap = () => { 
-    mapToggle.addEventListener("click", () => {
-        console.log("map clicked");
+
+const initToggleMap = (producerList) => { //when list clicked, map d-none
+    listToggle.addEventListener(("click"),() => {
+        //producerList.classList.add("d-none");
+    });
+    mapToggle.addEventListener(("click"), (producerList) => {
+        //producerList.classList.add("d-none");
     });
 }
 
