8f8998c6d7d007dca87337bd9d4f8d6114d02fe9
Lily Tan <lilyt2995@gmail.com>
2020-09-16 12:44:56 +1000

    FEAT: toggle map / list mechanic complete

--- app/javascript/components/init_toggle_map.js
+++ app/javascript/components/init_toggle_map.js
@@ -2,15 +2,24 @@ const mapToggle = document.querySelector(".display-map");
 const listToggle = document.querySelector(".display-list");
 const producerList = document.getElementById("js-producers")
 const map = document.getElementById("js-map")
+const active = document.querySelector("active");
 
+const initToggleMap = () => { //when list clicked, map d-none
+    mapToggle.addEventListener(("click"), () => {
+        producerList.classList.add("d-none");
+        map.classList.remove("d-none");
+        mapToggle.classList.add("active");
+        listToggle.classList.remove("active");
+    });
 
-const initToggleMap = (producerList) => { //when list clicked, map d-none
     listToggle.addEventListener(("click"),() => {
-        //producerList.classList.add("d-none");
+       map.classList.add("d-none");
+       producerList.classList.remove("d-none");
+       mapToggle.classList.remove("active");
+       listToggle.classList.add("active");
     });
-    mapToggle.addEventListener(("click"), (producerList) => {
-        //producerList.classList.add("d-none");
-    });
+
 }
+
 
 export { initToggleMap }
