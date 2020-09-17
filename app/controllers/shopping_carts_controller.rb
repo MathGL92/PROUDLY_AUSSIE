6888a916926193d7b5df4c03d1dab49a5253331b
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-17 14:08:32 +1000

    initialiazed

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,6 +1,4 @@
 class ShoppingCartsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
-  def show
-    # @line_items = @shopping_cart.line_items
-  end
+  def show; end
 end
