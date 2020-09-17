e64bceca7c9370b7f4f0bab3c3e0849cc4143f06
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-17 13:32:35 +1000

    add items done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,15 @@ Rails.application.routes.draw do
   resource :shopping_carts, only: [:show]
 
   resources :producers, only:[:index, :show, :new, :create]
+
+  # post '/add_to_cart/:product_id' => 'shopping_carts#add_to_cart', :as => 'add_to_cart'
+
+  namespace :shopping_cart do
+    resources :products, only: [] do
+      resources :line_items, only: :create
+    end
+  end
+
   get "/new_producer", to: "pages#new_producer"
   get "/producer_confirmation", to: "pages#producer_confirmation"
 
