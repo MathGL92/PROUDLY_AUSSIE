b0cc94ff711dbf96e00ec75862fb7240b539fed7
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-17 17:50:38 +1000

    trying to come back to previous version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
-  # def set_shopping_cart
-  #   @shopping_cart = current_user.shopping_cart
-  # end
-  # before_action :current_shopping_cart
+  before_action :current_shopping_cart
 
-  # def user_current
-  #   if session[:user_id]
-  #     @user = User.find(session[:user_id])
-  #   end
-  # end
+  def user_current
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
+  end
 
-  # def current_shopping_cart
-  #   if login?
-  #     @shopping_cart = @user.shopping_cart
-  #   else
-  #     if session[:shopping_cart]
-  #       @shopping_cart = ShoppingCart.find(session[:shopping_cart])
-  #     else
-  #       @shopping_cart = ShoppingCart.create
-  #       session[:shopping_cart] = @shopping_cart.id
-  #     end
-  #   end
-  # end
+  def current_shopping_cart
+    if login?
+      @shopping_cart = @user.shopping_cart
+    else
+      if session[:shopping_cart]
+        @shopping_cart = ShoppingCart.find(session[:shopping_cart])
+      else
+        @shopping_cart = ShoppingCart.create
+        session[:shopping_cart] = @shopping_cart.id
+      end
+    end
+  end
 
-  # def login?
-  #   !!user_current
-  # end
+  def login?
+    !!user_current
+  end
 end
--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -1,6 +1,4 @@
 class ShoppingCartsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:show]
-  def show
-    @shopping_cart = current_user.shopping_cart
-  end
+  def show; end
 end
