823ae50ddc300dba8210737dd041cdf5f702e90d
Matt McCrystal <mccrystal.matthew@gmail.com>
2020-09-17 10:55:36 +1000

    pulled latest master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_053427) do
+ActiveRecord::Schema.define(version: 2020_09_15_070430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_053427) do
 
   create_table "shopping_carts", force: :cascade do |t|
     t.boolean "paid"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_shopping_carts_on_user_id"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 2020_09_16_053427) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
