4af1758762462e907e655e6b6d242e92cb246122
Bence Fulop <fbence90@gmail.com>
2020-09-20 13:17:10 +1000

    ARCH: can add a single tag to product on new page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
     else
       render :new
     end
-  end
+  end  
 
   private
 
--- app/controllers/tagging_controller.rb
+++
@@ -1,2 +0,0 @@
-class TaggingController < ApplicationController
-end
