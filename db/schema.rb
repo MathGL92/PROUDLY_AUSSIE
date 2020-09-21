849ca0dde3b03c907775dec7fbc299c76abca7bf
Mathieu Longe <mathieu.longe@orange.fr>
2020-09-21 18:21:22 +1000

    initializing branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_09_21_002426) do
 
   # These are extensions that must be enabled in order to support this database
@@ -98,7 +97,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_002426) do
     t.bigint "tag_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "producer_id"
     t.index ["product_id"], name: "index_taggings_on_product_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
