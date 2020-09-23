2db5949b67d7cbfc3ee52d4d46366a8a1c909c06
Bence Fulop <fbence90@gmail.com>
2020-09-23 11:29:12 +1000

    selectize working no styling at the moment

---
+++ app/javascript/components/selectize.js
@@ -0,0 +1,20 @@
+import 'selectize/dist/js/selectize.min.js';
+import 'selectize/dist/css/selectize.css';
+
+const initTagging = () => {
+  if (document.getElementById('tag-field')) {
+    $('#tag-field').selectize({
+      plugins: ['remove_button'],
+      delimiter: ',',
+      persist: false,
+      create: function (input) {
+        return {
+          value: input,
+          text: input,
+        };
+      },
+    });
+  }
+};
+
+export { initTagging };
